c9a1e896d9920e0b8500ebde996d7fececb6c4b8
vipin <vipin@nexus.ethz.ch>
2017-05-31 14:46:13 +0200

    experiment tracking design

---
+++ track_screen_exp_wf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+The tracking of screening experiment from multiple steps 
+
+Copyright (C)
+
+2017 - ETH Zürich, NEXUS Personalized Health Technologies
+
+"""
+
+import os 
+import sys
+
