2401f0bf79c5e2a2d41ae6dba9abe4658745dc4c
vipin <vipin@nexus.ethz.ch>
2017-06-13 16:46:57 +0200

    two functions are ready now

--- track_screen_exp_wf.py
+++ track_screen_exp_wf.py
@@ -23,6 +23,7 @@ stock_name = "Kinase"
 
 experiment_path = "/Users/vipin/Documents/tdu_screens/"
 
+intermediate_file = "%s/exp-setup/VI000821.tab.csv" % experiment_path
 ## From here on, program aims to search multiple folders to scan intermediate files
 ## Based on the above mentioned stock name, it will generate the first mapping to the 
 ## target. From there it will look for the remaining associations. 
@@ -33,10 +34,10 @@ experiment_path = "/Users/vipin/Documents/tdu_screens/"
 
 def csv_data_loader(file_name):
 	"""
-	load full data from the experiment step in a dataframe. 
+	load full data from the csv file, returns a dataframe. 
 	
-	@args filename: csv file generated in a screening experiment
-	@args type: str 
+	@args file_name: csv file generated in a screening experiment
+	@type file_name: str 
 	"""
     
 	exp_details = pandas.read_csv(file_name, header=0)
@@ -45,34 +46,74 @@ def csv_data_loader(file_name):
 	return exp_df 
 
 
-intermediate_file = "%s/exp-setup/VI000821.tab.csv" % experiment_path
+def barcode_identifier(raw_df):
+    """
+    extract the source and destination barcodes from a dataframe and returns 
+    two lists. 
+
+    @args raw_df: experiment details from csv file
+    @type raw_df: pandas dataframe
+    """
+
+    ## filter the dataframe columns for barcode information 
+    col_names = [] 
+
+    ## general keyword describing the barcode  
+    keywords = ["SourceBarcode", "DestinationBarcode"]
+
+    for cols in raw_df: 
+        try:
+            ## keyword match is exact now and need to replaced with wild 
+            ## TODO something like x.str.contains("word")
+            if raw_df[cols].apply(lambda x: str(x) in keywords).any():
+                col_names.append(cols) 
+        except:
+            pass 
+
+    #print(col_names)
+    ## now the from the selected column, get the barcodes
+    src_barcodes = [] 
+    dst_barcodes = [] 
+
+    ## selection of barcodes after the matching of keyword 
+    for ind_col in col_names:
+        ## initializing the flags 
+        src_bc_tag = 0 
+        dst_bc_tag = 0 
+        for element in raw_df[ind_col]:
+            if not element:
+                ## empty lines are re-initializing the flags 
+                src_bc_tag = 0 
+                dst_bc_tag = 0 
+                continue 
+            ## loading the barcode values 
+            if src_bc_tag:
+                src_barcodes.append(element)  
+                continue
+            if dst_bc_tag:
+                dst_barcodes.append(element)
+                continue
+            ## checking the barcode types
+            if element in "DestinationBarcode":
+                dst_bc_tag = 1 
+            if element in "SourceBarcode":
+                src_bc_tag = 1
+
+    #print src_barcodes
+    #print dst_barcodes
+    return(src_barcodes, dst_barcodes)
+
 
 ## read the file
-raw_df = csv_data_loader(intermediate_file)
+csv_df = csv_data_loader(intermediate_file)
 
-## Looping through the dataframe columns will give the list of columns to be looked  
-col_names = [] 
+## get the barcodes
+src_bc, dst_bc = barcode_identifier(csv_df)
 
-for cols in raw_df: 
-	try:
-		## keyword match is exact now and need to replaced with wild TODO something like x.str.contains("word")
-		if raw_df[cols].apply(lambda x: "SourceBarcode" in str(x)).any() | \
-			raw_df[cols].apply(lambda x: "DestinationBarcode" in str(x)).any():
-			col_names.append(cols) 
-	except:
-		pass 
+## TODO search for the files in a specified path
+## csv or tab 
 
-print col_names
-
-## selection of barcodes after the label
-
-
-print raw_df[col_names[1]]
+print src_bc 
+print dst_bc
 
 sys.exit(-1)
-
-
-
-print exp_df.iloc[:,[1,4]]
-## search for the name SourceBarcode and DestinationBarcode from the dataframe selected columns 
- 
